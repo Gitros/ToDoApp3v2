3d3b7509ad8e52bab700dd5797afc9eefa3a2e2c
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-11-08 16:37:50 +0100

    Application CQRS approach

---
+++ ToDoAppBackend/ToDo.Application/Tasks/Queries/GetTasksQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Domain;
+using ToDo.Infrastructure;
+
+namespace ToDo.Application.Tasks.Queries;
+
+public record GetTasksQuery(Domain.TaskStatus? Status = null) : IRequest<List<TaskItem>>;
+
+public class GetTasksHandler : IRequestHandler<GetTasksQuery, List<TaskItem>>
+{
+    private readonly ToDoDbContext _db;
+    public GetTasksHandler(ToDoDbContext db) => _db = db;
+
+    public async Task<List<TaskItem>> Handle(GetTasksQuery q, CancellationToken ct)
+    {
+        var query = _db.Tasks.AsNoTracking().Where(t => !t.IsDeleted);
+        if (q.Status.HasValue) query = query.Where(t => t.Status == q.Status);
+        return await query.ToListAsync(ct);
+    }
+}
+
